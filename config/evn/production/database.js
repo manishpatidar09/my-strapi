341fa94f91b83f23dc5243431d17542125f414c5
manishpatidar09 <manishpatidar09@gmail.com>
2025-02-28 11:45:24 +0530

    dataabse

--- config/evn/production/database.js
+++ config/evn/production/database.js
@@ -1,5 +1,6 @@
 const parse = require('pg-connection-string').parse;
-    const config = parse(process.env.postgres://u9duj7dfju11vg:pbf7c8ecefeecbfe5439a73777e7bcfa048b14554b2ae5a86ed748491e2285c8c@cc0gj7hsrh0ht8.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d6g7fpmuefahfr);
+    const DATABASE_URL = "postgres://u9duj7dfju11vg:pbf7c8ecefeecbfe5439a73777e7bcfa048b14554b2ae5a86ed748491e2285c8c@cc0gj7hsrh0ht8.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d6g7fpmuefahfr";
+    const config = parse(process.env.DATABASE_URL);
     module.exports = ({ env }) => ({
       connection: {
         client: 'postgres',
